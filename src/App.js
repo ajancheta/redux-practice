a8c2c7285db1a9257005a3df774684dd50724b43
AJ Ancheta and Isaac Hall <isaac@>
2019-07-16 11:45:01 -0700

    add input field, update live state with redux

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar.js';
 import './App.css';
 import { connect } from 'react-redux';
 import { updateUser } from './actions/user-actions';
+import { bindActionCreators } from 'redux';
 
 class App extends Component {
   constructor(props){
@@ -12,9 +13,9 @@ class App extends Component {
     this.onUpdateUser = this.onUpdateUser.bind(this);
   }
 
-  onUpdateUser() {
+  onUpdateUser(event) {
     console.log(this.props);
-    this.props.onUpdateUser('Timmy');
+    this.props.onUpdateUser(event.target.value);
   }
 
   render() {
@@ -23,19 +24,31 @@ class App extends Component {
         <div onClick={this.onUpdateUser}>
         Update User
         </div>
+        <input onChange={this.onUpdateUser} />
         {this.props.user}
       </div>
 
     );
   }
 }
-  const mapStateToProps = state => ({
-    products: state.products,
-    user: state.user
-  });
-
-  const mapActionstoProps = {
-    onUpdateUser: updateUser
+  const mapStateToProps = (state, props) => {
+    console.log(props);
+    return {
+      products: state.products,
+      user: state.user,
+      userPlusProp: `${state.user} ${props.aRandomProps}`
+    }
   };
 
+  const mapActionstoProps  = (dispatch, props) =>  {
+    return bindActionCreators({
+      onUpdateUser: updateUser
+    }, dispatch);
+  };
+
+
+// const mergeProps = (propsFromState, propsFromDispatch, ownProps) => {
+//   console.log(propsFromState, propsFromDispatch,  ownProps)
+//   return {};
+// }
 export default connect(mapStateToProps, mapActionstoProps)(App);
